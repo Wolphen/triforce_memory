f5e853a73c35dae1c1f811caeb41ec2e1dfc4019
Wolphen <enzo.grignon@edu.esiee.fr>
2023-10-25 16:52:12 +0200

    story 9 and under

--- connexion.php
+++ connexion.php
@@ -3,17 +3,22 @@
 <html lang="fr">
 <?php require 'partials/head.php';?>
 <?php require 'partials/heade.php';?>
+<?php require 'utils/database.php';?>
 <body>
 
    <div class="banniere">
         <h1>C O N N E X I O N</h1>
     </div>
+
+    <?php if (isset($_GET['emailUser'])){
+                connexionUser( ($_GET['emailUser']), ($_GET['passwrdCo']));
+    }
+    ?>
 <!-- Code de connexion ci dessous -->
-    <form class = "formm">
-        <input type="text" id="mail" name="mail" placeholder="Email" required class ="inputtexte" >
+    <form class = "formm" method ="GET">
+        <input type="text" id="mail" name="emailUser" placeholder="Email" required class ="inputtexte" >
         <br>
-        <input type="text" id="password" placeholder="Mot de passe" required class ="inputtexte" >
-        
+        <input type="password" id="password"name="passwrdCo" placeholder="Mot de passe" required class ="inputtexte" >
         <br>
         <input type="submit" value="Connexion" class ="inputsubmit">  
          <a href="inscription.php"><input type="button" value="Pas de compte ? Crée un compte ici." class ="creecompte"></a>
--- inscription.php
+++ inscription.php
@@ -4,23 +4,25 @@
 <html lang="fr">
 <?php require 'partials/head.php'; ?>
 <?php require 'partials/heade.php'; ?>
-
 <?php
+$email = $_GET['email'] ?? '';
+$pseudo = $_GET['pseudo'] ?? '';
+$passwrd = $_GET['passwrd'] ?? '';
+$passwordConfirm = $_GET['passwordConfirm'] ?? '';
 
 
-    try{
-        if (isset($_GET['mail']) && $_GET['mail'] != "" )
-        checkMailValidity($_GET["mail"]);
-    } 
-    catch(Exception $e){
-         $feedback=$e->getMessage();
+
+try {
+    if (isset($_GET['email'], $_GET['pseudo'],$_GET['passwrd'],$_GET['passwordConfirm'])) {
+       subscribeForm($email, $pseudo, $passwrd, $passwordConfirm);
     }
-
-
-
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
 
 
 ?>
+
 <body>
 
 
@@ -28,26 +30,27 @@
         <h1>INSCRIPTION</h1>
     </div>
     <!-- Form pour demander l'inscription de la personne via un mail -->
-    <form class="forms" method="get">
-        <label for="mail" class="labell"></label>
-        <input type="text" id="mail" name="mail" placeholder="Email" required class="inputl">
-       <?php if (isset($_GET['mail']) && $_GET['mail'] != "" ) 
-       echo $feedback
-       ?>
-        
-        <br>
-        <input type="text" id="pseudo" name="pseudo" placeholder="Pseudo" required class="inputl">
-        <br>
-        <label for="password" class="labell"></label>
-        <input type="text" id="password" name="password" placeholder="Mot de passe" required class="inputl">
-        <br>
-        <label for="password" class="labell"></label>
-        <input type="text" id="password" name="password" placeholder="Confirmer le mot de passe" required class="inputl">
+    <div class="forms">
+        <form method="get">
+            <label for="mail" class="labell"></label>
+            <input type="text" id="email" name="email" placeholder="Email" required class="inputl">
 
-        <br>
-        <input type="submit" value="inscription" class="inputsubmit">
-    </form>
-    <a href="connexion.php"><input type="button" value="Déjà un compte ? Connectez-vous ici." class="creecompte"></a>
+            <br>
+            <input type="text" id="pseudo" name="pseudo" placeholder="Pseudo" required class="inputl">
+
+            <br>
+            <label for="password" class="labell"></label>
+            <input type="text" id="password" name="passwrd" placeholder="Mot de passe" required class="inputl">
+            <br>
+            <label for="password" class="labell"></label>
+            <input type="text" id="password" name="passwordConfirm" placeholder="Confirmer le mot de passe" required class="inputl">
+
+            <br>
+            <input type="submit" value="inscription" class="inputsubmit">
+            <label class="creecompte"><a href="connexion.php">Déjà un compte ? Connectez-vous ici.</a></label>
+            </label>
+    </div>
+
 
     <?= require_once SITE_ROOT . "partials/footer.php"; ?>
 
